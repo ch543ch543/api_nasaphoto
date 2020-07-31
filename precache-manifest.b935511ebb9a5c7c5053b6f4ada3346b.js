9638dfaf047d132a5c4c8f2c13ce115bd4d2e5b4
Yichun Tsai <ch543ch543@gmail.com>
2020-07-31 13:21:14 +0800

    Updates

---
+++ precache-manifest.b935511ebb9a5c7c5053b6f4ada3346b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2995df1246434726985557517176aa55",
+    "url": "/api_nasaphoto/index.html"
+  },
+  {
+    "revision": "3715775c4dc791bc817b",
+    "url": "/api_nasaphoto/static/css/main.92abd8bb.chunk.css"
+  },
+  {
+    "revision": "6e304f98313b071d4fcb",
+    "url": "/api_nasaphoto/static/js/2.910897da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/api_nasaphoto/static/js/2.910897da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3715775c4dc791bc817b",
+    "url": "/api_nasaphoto/static/js/main.f2234946.chunk.js"
+  },
+  {
+    "revision": "d45415e151cbcbf6b427",
+    "url": "/api_nasaphoto/static/js/runtime-main.492be63d.js"
+  }
+]);
